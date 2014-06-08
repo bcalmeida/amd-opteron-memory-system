bf4096612a30c630f5429bbebf462b3c637d8064
Bruno Almeida <bruno.correia.almeida@gmail.com>
2014-06-08 16:57:50 -0300

    Add more tests

--- src/extra/Helper.java
+++ src/extra/Helper.java
@@ -2,13 +2,17 @@ package extra;
 
 public class Helper {
 
+    public static boolean LOGGING = false;
+
     public static void errorLog(Object caller, String error) {
         String className = caller.getClass().getSimpleName();
         System.out.println("ERROR at " + className + ": " + error);
     }
 
     public static void log(Object caller, String text) {
-        String className = caller.getClass().getSimpleName();
-        System.out.println("LOG at " + className + ": " + text);
+        if(LOGGING) {
+            String className = caller.getClass().getSimpleName();
+            System.out.println("LOG at " + className + ": " + text);
+        }
     }
 }
--- src/extra/Test.java
+++ src/extra/Test.java
@@ -4,17 +4,54 @@ import memory.MemorySystem;
 
 public class Test {
 
-    // Test memory with 1 cache level reading operation
     public static void main(String[] args) {
         MemorySystem mem = new MemorySystem();
+
+        // Reading
+        System.out.println("Testing reading same address");
         mem.read("0041f7a0");
         mem.read("0041f7a0");
         mem.read("0041f7a2");
         mem.read("004758a0");
+        System.out.println("Situation: " + mem);
+        System.out.println("Should be: " + "Cache L1 : 0041f7 : 004758");
+        System.out.println();
+
+        System.out.println("Testing reading multiple address and checking if the oldest block is removed");
         mem.read("0041f7a0");
         mem.read("1041f7a0");
         mem.read("2041f7a0");
         mem.read("3041f7a0");
-        System.out.println(mem);
+        System.out.println("Situation: " + mem);
+        System.out.println("Should be: " + "Cache L1 : 004758 : 1041f7 : 2041f7 : 3041f7");
+        System.out.println();
+
+        // Writing
+        System.out.println("Testing write hit");
+        mem.write("1041f7a0", "00000000");
+        mem.write("2041f7a0", "00000000");
+        mem.write("2041f7a0", "00000001");
+        System.out.println("Situation: " + mem);
+        System.out.println("Should be: " + "Cache L1 : 004758 : 1041f7(*) : 2041f7(*) : 3041f7");
+        System.out.println();
+
+        System.out.println("Testing write miss");
+        mem.write("AA00f7a0", "00000000");
+        mem.write("AA00f7a0", "00000001");
+        System.out.println("Situation: " + mem);
+        System.out.println("Should be: " + "Cache L1 : 1041f7(*) : 2041f7(*) : 3041f7 : AA00f7(*)");
+        System.out.println();
+
+        System.out.println("Testing if dirty blocks are written on main memory");
+        Helper.LOGGING = true;
+        mem.write("AA10f7a0", "00000000");
+        System.out.println();
+
+        mem.read("AA20f7a0");
+        System.out.println();
+        Helper.LOGGING = false;
+        System.out.println("Situation: " + mem);
+        System.out.println("Should be: " + "Cache L1 : 3041f7 : AA00f7(*) : AA10f7(*) : AA20f7");
+        System.out.println();
     }
 }
