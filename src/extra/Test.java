a59b519cf879e99563888b9807117900b9f2251f
Bruno Almeida <bruno.correia.almeida@gmail.com>
2014-06-09 00:23:32 -0300

    Create new tests

--- src/extra/Test.java
+++ src/extra/Test.java
@@ -7,51 +7,72 @@ public class Test {
     public static void main(String[] args) {
         MemorySystem mem = new MemorySystem();
 
-        // Reading
-        System.out.println("Testing reading same address");
-        mem.read("0041f7a0");
-        mem.read("0041f7a0");
-        mem.read("0041f7a2");
-        mem.read("004758a0");
-        System.out.println("Situation: " + mem);
-        System.out.println("Should be: " + "Cache L1 : 0041f7 : 004758");
-        System.out.println();
+        System.out.println("Filling caches");
+        mem.read("01000000");
+        mem.read("01000000");
+        mem.read("02000000");
+        mem.read("03000000");
+        mem.read("04000000");
+        // testOutput(mem, "Cache L2, Cache L1 : 010000 : 020000 : 030000 : 040000");
+        testOutput(mem, "Cache L2, Cache L1 : 40000 : 80000 : c0000 : 100000");
+        mem.read("05000000");
+        mem.read("06000000");
+        mem.read("07000000");
+        mem.read("08000000");
+        mem.read("09000000");
+        // testOutput(mem, "Cache L2 : 010000 : 020000 : 030000 : 040000 : 050000, Cache L1 : 060000 : 070000 : 080000 : 090000");
+        testOutput(mem, "Cache L2 : 40000 : 80000 : c0000 : 100000 : 140000, Cache L1 : 180000 : 1c0000 : 200000 : 240000");
+        mem.read("10000000");
+        mem.read("11000000");
+        mem.read("12000000");
+        mem.read("13000000");
+        // testOutput(mem, "Cache L2 : 040000 : 050000 : 060000 : 070000 : 080000 : 090000, Cache L1 : 100000 : 110000 : 120000 : 130000");
+        testOutput(mem, "Cache L2 : 100000 : 140000 : 180000 : 1c0000 : 200000 : 240000, Cache L1 : 400000 : 440000 : 480000 : 4c0000");
 
-        System.out.println("Testing reading multiple address and checking if the oldest block is removed");
-        mem.read("0041f7a0");
-        mem.read("1041f7a0");
-        mem.read("2041f7a0");
-        mem.read("3041f7a0");
-        System.out.println("Situation: " + mem);
-        System.out.println("Should be: " + "Cache L1 : 004758 : 1041f7 : 2041f7 : 3041f7");
-        System.out.println();
+        System.out.println("Reading from cache L1");
+        mem.read("11000000");
+        // testOutput(mem, "Cache L2 : 040000 : 050000 : 060000 : 070000 : 080000 : 090000, Cache L1 : 100000 : 110000 : 120000 : 130000");
+        testOutput(mem, "Cache L2 : 100000 : 140000 : 180000 : 1c0000 : 200000 : 240000, Cache L1 : 400000 : 440000 : 480000 : 4c0000");
 
-        // Writing
-        System.out.println("Testing write hit");
-        mem.write("1041f7a0", "00000000");
-        mem.write("2041f7a0", "00000000");
-        mem.write("2041f7a0", "00000001");
-        System.out.println("Situation: " + mem);
-        System.out.println("Should be: " + "Cache L1 : 004758 : 1041f7(*) : 2041f7(*) : 3041f7");
-        System.out.println();
+        System.out.println("Reading from cache L2");
+        mem.read("07000000");
+        // testOutput(mem, "Cache L2 : 040000 : 050000 : 060000 : 080000 : 090000 : 100000, Cache L1 : 110000 : 120000 : 130000 : 070000");
+        testOutput(mem, "Cache L2 : 100000 : 140000 : 180000 : 200000 : 240000 : 400000, Cache L1 : 440000 : 480000 : 4c0000 : 1c0000");
+        mem.read("10000000");
+        //testOutput(mem, "Cache L2 : 040000 : 050000 : 060000 : 080000 : 090000 : 110000, Cache L1 : 120000 : 130000 : 070000 : 100000");
+        testOutput(mem, "Cache L2 : 100000 : 140000 : 180000 : 200000 : 240000 : 440000, Cache L1 : 480000 : 4c0000 : 1c0000 : 400000");
 
-        System.out.println("Testing write miss");
-        mem.write("AA00f7a0", "00000000");
-        mem.write("AA00f7a0", "00000001");
-        System.out.println("Situation: " + mem);
-        System.out.println("Should be: " + "Cache L1 : 1041f7(*) : 2041f7(*) : 3041f7 : AA00f7(*)");
-        System.out.println();
+        System.out.println("Reading from main memory");
+        mem.read("14000000");
+        // testOutput(mem, "Cache L2 : 050000 : 060000 : 080000 : 090000 : 110000 : 120000, Cache L1 : 130000 : 070000 : 100000 : 140000");
+        testOutput(mem, "Cache L2 : 140000 : 180000 : 200000 : 240000 : 440000 : 480000, Cache L1 : 4c0000 : 1c0000 : 400000 : 500000");
 
-        System.out.println("Testing if dirty blocks are written on main memory");
-        Helper.LOGGING = true;
-        mem.write("AA10f7a0", "00000000");
-        System.out.println();
+        System.out.println("Writing on cache L1");
+        mem.write("14000000", "00000000");
+        mem.write("07000000", "00000000");
+        // testOutput(mem, "Cache L2 : 050000 : 060000 : 080000 : 090000 : 110000 : 120000, Cache L1 : 130000 : 070000(*) : 100000 : 140000(*)");
+        testOutput(mem, "Cache L2 : 140000 : 180000 : 200000 : 240000 : 440000 : 480000, Cache L1 : 4c0000 : 1c0000(*) : 400000 : 500000(*)");
 
-        mem.read("AA20f7a0");
-        System.out.println();
-        Helper.LOGGING = false;
-        System.out.println("Situation: " + mem);
-        System.out.println("Should be: " + "Cache L1 : 3041f7 : AA00f7(*) : AA10f7(*) : AA20f7");
-        System.out.println();
+        System.out.println("Writing on cache L2");
+        mem.write("06000000", "00000000");
+        // testOutput(mem, "Cache L2 : 050000 : 080000 : 090000 : 110000 : 120000 : 130000, Cache L1 : 070000(*) : 100000 : 140000(*) : 060000(*)");
+        testOutput(mem, "Cache L2 : 140000 : 200000 : 240000 : 440000 : 480000 : 4c0000, Cache L1 : 1c0000(*) : 400000 : 500000(*) : 180000(*)");
+
+        System.out.println("Checking if dirty block is stored on memory");
+        mem.read("20000000");
+        mem.read("21000000");
+        mem.read("22000000");
+        mem.read("23000000");
+        mem.read("24000000");
+        mem.read("25000000");
+        mem.read("26000000");
+        // testOutput(mem, "Cache L2 : 100000 : 140000(*) : 060000(*) : 200000 : 210000 : 220000, Cache L1 : 230000 : 240000 : 250000 : 260000");
+        testOutput(mem, "Cache L2 : 400000 : 500000(*) : 180000(*) : 800000 : 840000 : 880000, Cache L1 : 8c0000 : 900000 : 940000 : 980000");
+    }
+
+    private static void testOutput(MemorySystem mem, String expected) {
+        if (mem.toString().equals(expected)) {
+            System.out.println("PASS");
+        } else System.out.println("FAILED");
     }
 }
